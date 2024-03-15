d63ab906b3b54e2fe35aa6685724c9a9f82705d2
jiwon317 <chlghqlt12@naver.com>
2024-03-15 17:58:04 +0900

    first init

---
+++ src/main/java/me/jiwon/scheduleserver/ScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package me.jiwon.scheduleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServerApplication.class, args);
+	}
+
+}
